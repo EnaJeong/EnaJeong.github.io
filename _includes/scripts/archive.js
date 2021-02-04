48c4362bd99ceb6cc6502ec8cf197c927fe1905e
HyeonJoo Jeong <gekritzel1007@gmail.com>
2021-02-04 19:31:44 +0900

    Add hashtag processing
    - connect category tag in page to archive

--- _includes/scripts/archive.js
+++ _includes/scripts/archive.js
@@ -1,44 +1,58 @@
-var focusedBtn = {
+const Archive = {
   lastFocusedBtn : document.querySelector(".category-button[data-category=all]"),
-  update : function(btn){
-    if (focusedBtn.lastFocusedBtn != btn){
-      focusedBtn.lastFocusedBtn.className = "category-button";
+
+  changeState(category) {
+    let doc = document;
+    let btn = doc.querySelector(`.category-button[data-category='${category}']`);
+
+    if (Archive.lastFocusedBtn == btn){
+      return;
     }
 
+    Archive.lastFocusedBtn.className = "category-button";
     btn.className = "category-button focused";
-    focusedBtn.lastFocusedBtn = btn;
+    Archive.lastFocusedBtn = btn;
+
+    let posts = doc.querySelectorAll(".archive-post");
+    switch (category) {
+      case 'all': {
+        for (let i=0, n=posts.length; i < n; i++){
+          posts[i].className = "archive-post";
+        }
+        break;
+      }
+      default: {
+        for (let i=0, n=posts.length; i < n; i++){
+          if (posts[i].getAttribute('data-category') != category){
+            posts[i].className = "archive-post not-included";
+          } else {
+            posts[i].className = "archive-post";
+          }
+        }
+      }
+    }
+
+    let groups = doc.querySelectorAll(".archive-group");
+    for (let i=0, n=groups.length; i < n; i++){
+      let groupId = "archive-" + groups[i].getAttribute('data-group');
+      let groupName = doc.getElementById(groupId);
+
+      let posts = groups[i].querySelectorAll(".archive-post:not(.not-included)");
+      if (posts.length == 0) {
+        groupName.className = "archive-group-name not-included";
+      } else {
+        groupName.className = "archive-group-name";
+      }
+    }
   }
 };
 
-var changePostState = function(category) {
-  let doc = document;
-  focusedBtn.update(doc.querySelector(".category-button[data-category='" + category + "']"));
-
-  let posts = doc.querySelectorAll(".archive-post");
-  if (category === 'all') {
-    posts.forEach(function(post){
-      post.className = "archive-post";
-    });
-  } else {
-    posts.forEach(function(post){
-      if (post.getAttribute('data-category') != category){
-        post.className = "archive-post not-included"
-      } else {
-        post.className = "archive-post";
-      }
-    });
-  }
-
-  let groups = doc.querySelectorAll(".archive-group");
-  groups.forEach(function(group){
-    let groupId = "archive-" + group.getAttribute('data-group');
-    let groupName = doc.getElementById(groupId);
-
-    let posts = group.querySelectorAll(".archive-post:not(.not-included)");
-    if (posts.length == 0) {
-      groupName.className = "archive-group-name not-included";
-    } else {
-      groupName.className = "archive-group-name";
+(function(){
+  let hash = window.location.hash.substr(1);
+  if (hash != ""){
+    let btn = document.querySelector(`.category-button[data-category='${hash}']`);
+    if (btn) {
+      Archive.changeState(hash);
     }
-  });
-}
+  }
+})();
